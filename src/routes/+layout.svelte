638a02472e76fb09ce31260e759c0ffb639d9138
Myslitel666 <tretyakov_02@bk.ru>
2025-07-13 15:33:51 +0500

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script>
+  import ThemeProvider from "svelte-elegant/ThemeProvider";
+  import Header from "./Header.svelte";
+</script>
+
+<ThemeProvider>
+  <div class="app">
+    <main>
+      <Header />
+      <slot />
+    </main>
+  </div>
+</ThemeProvider>
+
+<svelte:head>
+  <style>
+    @import url("https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap");
+
+    * {
+      font-family: "Roboto", sans-serif;
+      font-weight: 300;
+    }
+  </style>
+</svelte:head>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,43 @@
+<script>
+  import { ColorThemeSwitch } from "svelte-elegant";
+  import { goto } from "$app/navigation";
+  import Header from "svelte-elegant/Header";
+
+  import { themeStore, themeMode } from "svelte-elegant/stores";
+
+  let theme;
+
+  let logotypeFilter = "";
+  let svelteColor = "";
+  let elegantColor = "";
+
+  // Подписываемся на изменения темы
+  themeStore.subscribe((value) => {
+    theme = value; //Инициализация объекта темы
+
+    svelteColor = theme.palette.primary;
+    elegantColor = $themeMode === "light" ? "#383838" : "#fdfdfd";
+    logotypeFilter = $themeMode === "light" ? "brightness(80%)" : "";
+  });
+</script>
+
+<Header>
+  <button style:gap="0.5rem" onclick={() => goto("/about")}>
+    <img src="/dice-2.png" height="45px" width="45px" alt="logo" />
+    <p style:font-size="26px">
+      <span
+        style:color={svelteColor}
+        style:filter={logotypeFilter}
+        style:transition="all 0.3s"
+      >
+        Answer
+      </span>
+      <span style:color={elegantColor} style:transition="all 0.3s">
+        Generator
+      </span>
+    </p>
+  </button>
+  <div style:margin-left="auto" style:margin-right="1.25rem">
+    <ColorThemeSwitch />
+  </div>
+</Header>
